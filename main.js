aae157a3083050d563f3724a147a420df1deabe4
‪Ebrahim Amgad <ebrahimamgad110@gmail.com>
2025-09-29 18:39:44 +0300

    Add Scroll Spy

--- main.js
+++ main.js
@@ -32,3 +32,34 @@ links.forEach(link => {
 		this.classList.add('active');
 	});
 });
+
+const sections = {
+	services: {},
+	portfolio: {},
+	about: {},
+	pricing: {},
+	contact: {}
+};
+
+window.addEventListener('load', _ => {
+	for (const section in sections)
+		sections[section].offsetTop = document.querySelector(`.${section}`).offsetTop;
+});
+
+window.addEventListener('scroll', _ => {
+	const scrollYValue = scrollY + 97;
+	if (scrollYValue > sections['services'].offsetTop)
+		for (const section in sections) {
+			if (scrollYValue > sections[section].offsetTop)
+				links.forEach(link => { 
+					link.classList.remove('active');
+					links.forEach(link => {
+						if (link.textContent.toLowerCase() === section) link.classList.add('active');
+					});
+				});
+		}
+	else {
+		links.forEach(link => link.classList.remove('active'));
+		document.querySelector('nav li:first-of-type a').classList.add('active');
+	}
+});
